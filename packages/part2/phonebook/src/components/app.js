7d10786299dc29296801d3ee087b3c431bf8bd97
rubberdvcky <dvckyyyy@gmail.com>
2020-05-20 17:39:02 -0500

    phonebook part 2

---
+++ packages/part2/phonebook/src/components/app.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+export const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} already exists!`);
+      return;
+    }
+
+    const newPerson = {
+      id: persons.length,
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          Name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
---
+++ packages/part2/phonebook/src/components/content.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// export const Content = ({ parts }) => {
+//   return (
+//     <ul>
+//       {parts.map((part) => (
+//         <Part key={part.id} part={part}></Part>
+//       ))}
+//     </ul>
+//   );
+// };
