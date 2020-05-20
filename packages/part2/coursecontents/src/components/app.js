9722ee6fad7f5c2a53b1ce0265ab3caa4e8fb7ad
rubberdvcky <dvckyyyy@gmail.com>
2020-05-20 16:40:23 -0500

    course contents step9-10

--- packages/part2/coursecontents/src/components/app.js
+++ packages/part2/coursecontents/src/components/app.js
@@ -2,27 +2,51 @@ import React from 'react';
 import { Course } from './course';
 
 export const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      {courses.map((course) => {
+        return <Course key={course.id} course={course} />;
+      })}
+    </div>
+  );
 };
--- packages/part2/coursecontents/src/components/total.js
+++ packages/part2/coursecontents/src/components/total.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export const Total = ({ parts }) => {
-  return <p>Total of {parts.reduce((acc, part) => part.exercises + acc, 0)}</p>;
+  const total = parts.reduce((acc, part) => part.exercises + acc, 0);
+
+  return <p>Total of {total} exercises</p>;
 };
