2214fc57948676e6536893839c9567a6c61de6b2
rubberdvcky <dvckyyyy@gmail.com>
2020-05-20 19:39:12 -0500

    phonebook step6

--- packages/part2/phonebook/src/components/app.js
+++ packages/part2/phonebook/src/components/app.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Filter } from './filter';
 import { PhonebookForm } from './phonebook-form';
 import { PhonebookNumbers } from './phonebook-numbers';
 
 export const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123-456-789' },
-    { name: 'Hellas', number: '123-456-789' },
-    { name: 'Hell', number: '123-456-789' },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  const getPersons = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((res) => {
+        console.log(res.config.url, res.status, res.data);
+        setPersons(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(getPersons, []);
+
   const [filter, setFilter] = useState('');
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(filter.toLowerCase()),
   );
+
   return (
     <div>
       <h2>Filter</h2>
